cde9f8b816bbab48483e56be3f7faa46ad24b67d
Robin Lin <robin.lin99@yahoo.com>
2022-01-23 21:09:15 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Robin Lin on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
